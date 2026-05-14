8cbced8829356b8954c1ba783df0bea104231725
imamHasan <mdimamhasanihr@gmail.com>
2026-05-14 11:35:30 +0600

    perf: remove deprecated esbuild minify, keep esnext target

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'esnext',
+    cssMinify: true,
+    reportCompressedSize: true,
     rollupOptions: {
       output: {
         manualChunks: (id) => {
